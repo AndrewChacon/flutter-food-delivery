a9e8642518d54d1ee12229e9806dd4b9a2690836
AndrewChacon <andrew.b.chacon@gmail.com>
2024-12-01 14:31:55 -0500

    created menu-drawer, page for settings, and a sliver-app-bar, as well as enabled color mode inside of the settings page for light and dark mode

--- mobile/lib/pages/home_page.dart
+++ mobile/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/components/my_current_location.dart';
+import 'package:mobile/components/my_description_box.dart';
+import 'package:mobile/components/my_drawer.dart';
+import 'package:mobile/components/my_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +15,33 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home")),
+      // appBar: AppBar(
+      //   title: const Text("Home"),
+      //   backgroundColor: Theme.of(context).colorScheme.surface,
+      // ),
+      drawer: const MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+              title: const Text("Title"),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Divider(
+                    indent: 25,
+                    endIndent: 25,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  // current location
+                  const MyCurrentLocation(),
+
+                  // description box
+                  const MyDescriptionBox(),
+                ],
+              )),
+        ],
+        body: Container(color: Colors.blue),
+      ),
     );
   }
 }
